ae8d505361f1f124ee27eb49bde4b95844c7a675
euglazer <eugenestapler@gmail.com>
2016-05-02 14:44:55 -0700

    can replay keystroke recording

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
-import KeystrokeRecorder from './../npm/keystroke-recorder/index.js'
+import KeystrokeRecorder from 'keystroke-recorder'
 import jsonMarkup from 'json-markup'
 import copy from 'copy-to-clipboard'
 import loop from 'raf-loop'
@@ -24,34 +24,21 @@ $('#stop').click(function () {
   $(this).hide()
   $('#record').show()
   $('#recording-indicator').hide()
+
   var json = keystrokeRecorder.json
   var jsonFormatted = jsonMarkup(json)
   $('#json-data').html(jsonFormatted)
 
-  // clear replay pad
-  $('#replay-pad').val('')
-  var replayChars = []
+  $('#replay').click()
+})
 
-  var timer = new Tock({
-    countdown: true,
-    interval: 10,
-    callback: function () {
-      var currentMs = keystrokeRecorder.timeElapsed() - timer.lap()
-      if (json[0] && currentMs > json[0].ms) {
-        var obj = json.shift()
-        if (obj.key === 'Backspace') {
-          replayChars.pop()
-        } else if (obj.key === 'Enter') {
-          replayChars.push('\n')
-        } else {
-          replayChars.push(obj.key)
-        }
-        $('#replay-pad').val(replayChars.join(''))
-      }
-    }
+$('#replay').click(function () {
+  $('#replay').hide()
+  $('#replaying-indicator').show()
+  keystrokeRecorder.replay('#replay-pad').then(() => {
+    $('#replay').show()
+    $('#replaying-indicator').hide()
   })
-
-  timer.start(keystrokeRecorder.timeElapsed())
 })
 
 $('#copy-to-clipboard-btn').click(() => {
