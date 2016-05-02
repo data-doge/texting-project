f600676370626ce67e8d466ae304566d3cc57fac
euglazer <eugenestapler@gmail.com>
2016-05-02 14:50:59 -0700

    Updates

---
+++ index.js
@@ -0,0 +1,47 @@
+import $ from 'jquery'
+import KeystrokeRecorder from 'keystroke-recorder'
+import jsonMarkup from 'json-markup'
+import copy from 'copy-to-clipboard'
+import loop from 'raf-loop'
+import Tock from 'tocktimer'
+
+var keystrokeRecorder = new KeystrokeRecorder({
+  omittedKeys: ['Tab', 'Meta', 'Control', 'Alt', 'Shift']
+})
+
+$('#record').click(function () {
+  $('#record-pad').val('')
+  keystrokeRecorder.record()
+  $(this).hide()
+  $('#stop').show()
+  $('#recording-indicator').show()
+  $('#record-pad').focus()
+})
+
+$('#stop').click(function () {
+  $('#record-pad').val('')
+  keystrokeRecorder.stop()
+  $(this).hide()
+  $('#record').show()
+  $('#recording-indicator').hide()
+
+  var json = keystrokeRecorder.json
+  var jsonFormatted = jsonMarkup(json)
+  $('#json-data').html(jsonFormatted)
+
+  $('#replay').click()
+})
+
+$('#replay').click(function () {
+  $('#replay').hide()
+  $('#replaying-indicator').show()
+  keystrokeRecorder.replay('#replay-pad').then(() => {
+    $('#replay').show()
+    $('#replaying-indicator').hide()
+  })
+})
+
+$('#copy-to-clipboard-btn').click(() => {
+  var json = $('#json-data').text()
+  copy(json)
+})
